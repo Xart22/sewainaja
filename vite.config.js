6fc67fbb24cee95656592f67b53fd883783837a5
Xart22 <fajar.rn22@gmail.com>
2024-12-01 05:30:05 +0700

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/dashboard.js",
+                "resources/js/customer.js",
+                "resources/js/users.js",
+                "resources/js/tracking.js",
+                "resources/js/hardware.js",
+                "resources/js/data-permohonan.js",
+                "resources/js/form-laporan.js",
+            ],
             refresh: true,
         }),
     ],
